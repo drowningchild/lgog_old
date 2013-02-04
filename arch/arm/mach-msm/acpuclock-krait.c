36a93663e09b7ed1e6489be44157cee4c0a68a1e
Drowningchild <drowningchild@drowning-5520.(none)>
2013-02-03 18:11:14 -0600

    ADD: 8064 Overclock

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -52,6 +52,8 @@ static int PIF_CHECK;
 
 #define SECCLKAGD		BIT(4)
 
+#define FREQ_TABLE_SIZE    35
+
 static DEFINE_MUTEX(driver_lock);
 static DEFINE_SPINLOCK(l2_lock);
 
@@ -907,7 +909,7 @@ void acpuclk_set_vdd(unsigned int khz, int vdd_uv) {
 #endif	/* CONFIG_CPU_VOTALGE_TABLE */
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
